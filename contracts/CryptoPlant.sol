ca4bbeb414a998f1299b6f0ae96302dc24f7e903
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2021-12-05 13:33:29 -0300

    feat: creating ERC20 token

---
+++ contracts/CryptoPlant.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    constructor() ERC20("CryptoPlants", "PLANT") {
+        _mint(0xB0C052c271296f18Be342AcC3Ba8E3ACe9907d90, 10000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
